e6c51ccc7c86e3cae773332994b921bdf57cacb9
unawarexi <unawarexi@gmail.com>
2024-09-11 22:01:44 +0100

    ElevatedButton and Text theme updated

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:greenway_commerce/utils/theme/custom_themes/elevatedButton_theme.dart";
 import "package:greenway_commerce/utils/theme/custom_themes/text_theme.dart";
 
 class TAppTheme {
@@ -10,12 +11,16 @@ class TAppTheme {
       brightness: Brightness.light,
       primaryColor: Colors.blue,
       scaffoldBackgroundColor: Colors.white,
-      textTheme: TTextTheme.lightTextTheme());
+      textTheme: TTextTheme.lightTextTheme,
+      elevatedButtonTheme: TElevatedButtonTheme.lightElevatedButtonTheme);
+
+  // ---------- GENERAL DARK APLLICATION THEME ------------------ //
   static ThemeData darkTheme = ThemeData(
       useMaterial3: true,
       fontFamily: "Poppins",
       brightness: Brightness.dark,
       primaryColor: Colors.blue,
       scaffoldBackgroundColor: Colors.black,
-      textTheme: TTextTheme.darkTextTheme());
+      textTheme: TTextTheme.darkTextTheme,
+      elevatedButtonTheme: TElevatedButtonTheme.darkElevatedButtonTheme);
 }
