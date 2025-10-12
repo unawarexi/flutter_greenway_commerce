763eb98a23942d10f3c94d0ffd738780221d2c30
unawarexi <unawarexi@gmail.com>
2025-10-12 05:57:17 +0100

    initialized and setup firebase and environment constants

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -6,15 +6,39 @@
 // @dart = 2.13
 // ignore_for_file: type=lint
 
+import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:connectivity_plus/src/connectivity_plus_web.dart';
+import 'package:file_picker/_internal/file_picker_web.dart';
+import 'package:firebase_app_check_web/firebase_app_check_web.dart';
+import 'package:firebase_auth_web/firebase_auth_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_messaging_web/firebase_messaging_web.dart';
+import 'package:firebase_storage_web/firebase_storage_web.dart';
 import 'package:flutter_native_splash/flutter_native_splash_web.dart';
+import 'package:flutter_secure_storage_web/flutter_secure_storage_web.dart';
+import 'package:fluttertoast/fluttertoast_web.dart';
+import 'package:google_sign_in_web/google_sign_in_web.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:permission_handler_html/permission_handler_html.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  FirebaseFirestoreWeb.registerWith(registrar);
   ConnectivityPlusWebPlugin.registerWith(registrar);
+  FilePickerWeb.registerWith(registrar);
+  FirebaseAppCheckWeb.registerWith(registrar);
+  FirebaseAuthWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  FirebaseMessagingWeb.registerWith(registrar);
+  FirebaseStorageWeb.registerWith(registrar);
   FlutterNativeSplashWeb.registerWith(registrar);
+  FlutterSecureStorageWeb.registerWith(registrar);
+  FluttertoastWebPlugin.registerWith(registrar);
+  GoogleSignInPlugin.registerWith(registrar);
+  ImagePickerPlugin.registerWith(registrar);
+  WebPermissionHandler.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:greenway_commerce/firebase_options.dart';
 // import 'package:greenway_commerce/utils/constants/colors.dart';
 
 import "app.dart";
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
